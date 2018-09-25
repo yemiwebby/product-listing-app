d1a26dbdc39032efdeeb5ff2e6db98bd22f6891b
yemiwebby <yemiwebby@gmail.com>
2018-09-25 06:50:05 +0100

    Created components

---
+++ assets/js/components/IncreaseLikeCount.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Form, Button,Badge } from 'reactstrap'
+import { APP } from './util'
+import axios from 'axios';
+
+class IncreaseLikeCount extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            id: props.apartmentId,
+            count: props.likeCount,
+        };
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    async onSubmit(e) {
+        e.preventDefault();
+        this.state.count++;
+
+        axios.post(`${APP.BASE_URL}/${APP.APARTMENTS_URL}/${this.state.id}/count`).then(res => {
+            this.props.likeIncrease(res.data, this.state.id)
+        });
+    }
+
+    render() {
+        return (
+            <Form onSubmit={this.onSubmit}>
+                <Button type='submit' color="primary" outline>
+                    Favorite <Badge color="secondary">{ this.state.count }</Badge>
+                </Button>
+            </Form>
+        )
+    }
+}
+
+export default IncreaseLikeCount;
---
+++ assets/js/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render(){
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#"> Symfony React Product Listing</a>
+        </nav>
+    )
+    }
+}
+export default Navbar;
